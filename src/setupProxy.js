0ea0a52f6313924c3263ff95aa39fece48a6f420
toptecshare <eestimorol.ice9208g@gmail.com>
2022-06-28 20:37:37 +0800

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/api2"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "54.166.234.211",
       changeOrigin: true,
       pathRewrite: {
         "^/api2": "/api", // rewrite path
